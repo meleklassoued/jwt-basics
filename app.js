e49200ed4279f81bef460dfc8805986ef443130d
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-01-18 18:05:26 +0100

    use the routes

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
-
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const mainRouter = require("./routes/main");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
 
+app.use("/api/v1", mainRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
@@ -19,7 +20,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+      console.log(`Server is listening on port ${port}...`),
     );
   } catch (error) {
     console.log(error);
