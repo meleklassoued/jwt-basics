6cc9de2f7814d12a943813232c908abbb1d366cb
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-01-20 18:05:11 +0100

    create errors with http-status-code

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require("./custom-error");
+const BadRequestError = require("./bad-request");
+const UnauthenticatedError = require("./custom-error");
+
+module.exports = {
+  UnauthenticatedError,
+  BadRequestError,
+  CustomAPIError,
+};
