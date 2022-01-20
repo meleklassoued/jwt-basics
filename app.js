f874086586ef1178200a2940e0cfb434ece0d414
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-01-20 15:24:23 +0100

    change the port to 5000

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/api/v1", mainRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
